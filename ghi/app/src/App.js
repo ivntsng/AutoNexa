4f144a8f2b8472340e3ee81217451d8c028f2671
James Lee <j.lee1203@hotmail.com>
2023-06-05 16:48:03 -0700

    test test

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -5,49 +5,13 @@ import ManufacturerList from "./ManufacturerList";
 import { useState, useEffect } from "react";
 
 function App() {
-  const [service, setService] = useState([]);
-  const [sales, setSales] = useState([]);
-
-  async function loadService() {
-    const serviceUrl = "http://localhost:8080/api/service/";
-    const response = await fetch(serviceUrl);
-    if (response.ok) {
-      const data = await response.json();
-      setService(data.service);
-    }
-  }
-
-  async function loadSales() {
-    const salesUrl = "http://localhost:8090/api/sales/";
-    const response = await fetch(salesUrl);
-    if (response.ok) {
-      const data = await response.json();
-      setSales(data.sales);
-    }
-  }
-
-  useEffect(() => {
-    loadService();
-    loadSales();
-  }, []);
-
   return (
     <BrowserRouter>
       <Nav />
       <div className="container">
         <Routes>
           <Route path="/" element={<MainPage />} />
-          <Route path="/service">
-            <Route
-              index
-              element={
-                <ManufacturerList
-                  manufacturersList={service}
-                  getManufacturer={loadService}
-                />
-              }
-            />
-          </Route>
+          <Route path="manufacturerList" element={<ManufacturerList />} />
         </Routes>
       </div>
     </BrowserRouter>
