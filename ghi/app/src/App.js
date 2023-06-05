7c40ebae819414b325430cadceb32053933b0f99
casper <ivntsng@gmail.com>
2023-06-05 16:00:05 -0700

    App changes

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -1,40 +1,53 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import MainPage from './MainPage';
-import Nav from './Nav';
-import ManufacturerList from './ManufacturerList';
-import { useState, useEffect } from 'react'
-
-
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MainPage from "./MainPage";
+import Nav from "./Nav";
+import ManufacturerList from "./ManufacturerList";
+import { useState, useEffect } from "react";
 
 function App() {
-  const [service, setService] = useState([])
-  const [sales, setSales] = useState([])
-  async function loadService(){
-    const serviceUrl = "http://localhost:8080/api/service/"
-    const response = await fetch(serviceUrl)
-    if(response.ok){
-      const data = await response.json()
-      setService(data.service)
-    }
-    async function loadSales(){
-      const salesUrl = "http://localhost:8090/api/sales/"
-      const response = await fetch(salesUrl)
-      if(response.ok){
-        const data = await response.json()
-        setSales(data.sales)
-      }
-    }
+  const [service, setService] = useState([]);
+  const [sales, setSales] = useState([]);
 
+  async function loadService() {
+    const serviceUrl = "http://localhost:8080/api/service/";
+    const response = await fetch(serviceUrl);
+    if (response.ok) {
+      const data = await response.json();
+      setService(data.service);
+    }
   }
+
+  async function loadSales() {
+    const salesUrl = "http://localhost:8090/api/sales/";
+    const response = await fetch(salesUrl);
+    if (response.ok) {
+      const data = await response.json();
+      setSales(data.sales);
+    }
+  }
+
+  useEffect(() => {
+    loadService();
+    loadSales();
+  }, []);
+
   return (
     <BrowserRouter>
       <Nav />
       <div className="container">
         <Routes>
           <Route path="/" element={<MainPage />} />
-          <Route path="manufacturer">
-            <Route index element={<ManufacturersList manufacturersList={manufacturer} getManufacturer={getManufacturer} />} />
+          <Route path="/service">
+            <Route
+              index
+              element={
+                <ManufacturerList
+                  manufacturersList={service}
+                  getManufacturer={loadService}
+                />
+              }
+            />
+          </Route>
         </Routes>
       </div>
     </BrowserRouter>
