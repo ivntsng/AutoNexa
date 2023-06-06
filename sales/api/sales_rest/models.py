18c3ae8325c49ae652789f4bf7747074293ea5d5
James Lee <j.lee1203@hotmail.com>
2023-06-06 15:47:15 -0700

    Finished writing the models

--- sales/api/sales_rest/models.py
+++ sales/api/sales_rest/models.py
@@ -10,6 +10,7 @@ class Salesperson(models.Model):
         return f"{self.first_name} + ' ' + {self.last_name}"
 
 
+
 class Customer(models.Model):
     first_name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200)
@@ -21,3 +22,34 @@ class Customer(models.Model):
 
 
 
+class AutomobileVO(models.Model):
+    import_href = models.CharField(max_length=200, unique=True)
+    vin = models.CharField(max_length=200, unique=True)
+    sold = models.CharField(default=False)
+
+    def __str__(self):
+        return f"{self.import_href}"
+
+
+
+class Sale(models.Model):
+    price = models.PositiveBigIntegerField()
+
+    automobile = models.ForeignKey(
+        AutomobileVO,
+        related_name="sales",
+        on_delete=models.CASCADE
+    )
+    salesperson = models.ForeignKey(
+        Salesperson,
+        related_name="sales",
+        on_delete=models.CASCADE
+    )
+    customer = models.ForeignKey(
+        Customer,
+        related_name="sales",
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return f"{self.salesperson} {self.customer} {self.automobile}"
