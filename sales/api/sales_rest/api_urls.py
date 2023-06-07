634eaa52bee9114a1b6e1b33f8ee9f565511fa87
James Lee <j.lee1203@hotmail.com>
2023-06-07 14:32:46 -0700

    FIXED POLLER AND HARD FUNCTION

--- sales/api/sales_rest/api_urls.py
+++ sales/api/sales_rest/api_urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import api_list_salespeople
+from .views import api_list_salespeople, api_salespeople_detail, api_list_customers, api_customer_detail, api_list_sales
 
 urlpatterns = [
     path('salespeople/', api_list_salespeople, name="api_list_salespeople"),
+    path('salespeople/<int:id>', api_salespeople_detail, name="api_salespeople_detail"),
+    path('customers/', api_list_customers, name="api_list_customers"),
+    path('customers/<int:id>', api_customer_detail, name="api_customer_detail"),
+    path('sales/', api_list_sales, name="api_list_sales"),
 ]
