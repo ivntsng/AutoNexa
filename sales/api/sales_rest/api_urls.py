2bdf6308de802bf5c3156c1f1d877d15ca1e208e
James Lee <j.lee1203@hotmail.com>
2023-06-07 16:36:03 -0700

    test

--- sales/api/sales_rest/api_urls.py
+++ sales/api/sales_rest/api_urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import api_list_salespeople, api_salespeople_detail, api_list_customers, api_customer_detail, api_list_sales
+from .views import api_list_salespeople, api_salespeople_detail, api_list_customers, api_customer_detail, api_list_sales, api_sale_detail
 
 urlpatterns = [
     path('salespeople/', api_list_salespeople, name="api_list_salespeople"),
@@ -7,4 +7,5 @@ urlpatterns = [
     path('customers/', api_list_customers, name="api_list_customers"),
     path('customers/<int:id>', api_customer_detail, name="api_customer_detail"),
     path('sales/', api_list_sales, name="api_list_sales"),
+    path('sales/<int:id>', api_sale_detail, name="api_sale_detail"),
 ]
