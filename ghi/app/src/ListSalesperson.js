15bd9bab8bc1e34d9d77499ca325dbb201a00ad8
James Lee <j.lee1203@hotmail.com>
2023-06-08 14:09:46 -0700

    Finished list of salespeople component

--- ghi/app/src/ListSalesperson.js
+++ ghi/app/src/ListSalesperson.js
@@ -1,3 +1,42 @@
-export default function SalesPeople({ getPeople }) {
-  return <p>Sale people</p>;
+import { useEffect, useState } from 'react';
+
+function ListSalespeople() {
+  const [salespeople, setSalespeople] = useState([]);
+  const fetchData = async () => {
+    const response = await fetch("http://localhost:8090/api/salespeople/");
+  if (response.ok) {
+    const data = await response.json();
+    setSalespeople(data.salespeople);
+  } else {
+    console.error(response);
+  }}
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+
+    <table className="table table-striped">
+        <thead>
+        <h2>Salespeople</h2>
+            <tr>
+            <th>Employee ID</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            </tr>
+        </thead>
+        <tbody>
+            {salespeople.map(salesperson => {
+            return(
+                <tr key={salesperson.id}>
+                    <td>{salesperson.employee_id}</td>
+                    <td>{salesperson.first_name}</td>
+                    <td>{salesperson.last_name}</td>
+            </tr>
+            )})}
+        </tbody>
+    </table>
+  )
 }
+
+export default ListSalespeople;
