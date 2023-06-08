fb7ee84bcf1ec6cc02c4935b8769a3b83f3b858d
James Lee <j.lee1203@hotmail.com>
2023-06-08 11:16:21 -0700

    Finished customer list form

--- ghi/app/src/ListCustomers.js
+++ ghi/app/src/ListCustomers.js
@@ -1,28 +1,43 @@
 import {useEffect, useState} from 'react';
 
-function RecordSale() {
+function ListCustomers() {
+  const [customers, setCustomers] = useState([]);
+  const fetchData = async () => {
+    const response = await fetch("http://localhost:8090/api/customers/");
+  if (response.ok) {
+    const data = await response.json();
+    setCustomers(data.customers);
+  } else {
+    console.error(response);
+  }}
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+
   return (
-    <p>List of customers</p>
-    // <table className="table table-striped">
-    //     <thead>
-    //         <tr>
-    //         <th>First Name</th>
-    //         <th>Last Name</th>
-    //         <th>Phone Number</th>
-    //         <th>Address</th>
-    //         </tr>
-    //     </thead>
-    //     <tbody>
-    //         {customers.map(customer => {
-    //         return(
-    //             <tr key={customer.id}>
-    //                 <td>{customer.firstName}</td>
-    //                 <td>{customer.lastName}</td>
-    //                 <td>{customer.phoneNumber}</td>
-    //                 <td>{customer.address}</td>
-    //         </tr>
-    //         )})}
-    //     </tbody>
-    // </table>
+    <table className="table table-striped">
+        <thead>
+            <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Phone Number</th>
+            <th>Address</th>
+            </tr>
+        </thead>
+        <tbody>
+            {customers.map(customer => {
+            return(
+                <tr key={customer.id}>
+                    <td>{customer.first_name}</td>
+                    <td>{customer.last_name}</td>
+                    <td>{customer.phone_number}</td>
+                    <td>{customer.address}</td>
+            </tr>
+            )})}
+        </tbody>
+    </table>
   )
 }
+
+export default ListCustomers;
